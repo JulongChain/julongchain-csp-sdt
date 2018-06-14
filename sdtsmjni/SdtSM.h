481b90bdd67204428deca7feeb0763c91ae7fe19
tengxiumin <xmteng@163.com>
2018-06-14 16:30:03 +0800

    [BJC-125] 增加开源协议说明

--- sdtsmjni/SdtSM.h
+++ sdtsmjni/SdtSM.h
@@ -1,12 +1,27 @@
-/******************************************************
-file_name		 :  SdtSM.h												
-auther			 ：
-description      :  sm算法调用接口库头文件
-others			 ：  none
-modification：
-				  2018-04-27	created this file
-						
-******************************************************/
+/**
+ * Copyright SDT. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * SdtSM.h 算法接口库头文件
+ *
+ * @author tengxiumin
+ * @date 2018/05/22
+ * @company SDT
+ */
+
 #ifndef __SDTSM_H_
 #define __SDTSM_H_
 
